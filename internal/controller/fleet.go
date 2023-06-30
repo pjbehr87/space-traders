cd06360bc49d9bda8ee1880338de7333daa13f1e
Phil B <pjbehr87@gmail.com>
2023-06-30 01:00:25 -0600

    adding ship navigation

--- internal/controller/fleet.go
+++ internal/controller/fleet.go
@@ -17,6 +17,12 @@ type shipsPage struct {
 
 	Ships []stlib.Ship
 }
+type shipPage struct {
+	Page PageData
+
+	Ship      stlib.Ship
+	Waypoints []stlib.Waypoint
+}
 
 func NewFleetController(e *echo.Echo, stl stlib.StLib) {
 	cont := fleetController{
@@ -25,8 +31,11 @@ func NewFleetController(e *echo.Echo, stl stlib.StLib) {
 
 	e.Logger.Debug("Router added: Fleet")
 	e.POST("/my/ships", cont.purchaseShip)
-	e.GET("/my/ships", cont.getShips)
+	e.GET("/my/ships", cont.listShips)
 	e.GET("/my/ships/:shipSymbol", cont.getShip)
+	e.POST("/my/ships/:shipSymbol/orbit", cont.orbitShip)
+	e.POST("/my/ships/:shipSymbol/dock", cont.dockShip)
+	e.POST("/my/ships/:shipSymbol/navigate", cont.navigateShip)
 }
 
 func (ctl *fleetController) purchaseShip(c echo.Context) error {
@@ -37,16 +46,44 @@ func (ctl *fleetController) purchaseShip(c echo.Context) error {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	err = c.NoContent(http.StatusOK)
+	err = c.String(http.StatusOK, "{}")
+	if err != nil {
+		c.Logger().Error(err.Error())
+	}
+	return err
+}
+func (ctl *fleetController) orbitShip(c echo.Context) error {
+	c.Logger().Info(fmt.Sprintf("Request: POST OrbitShip shipSymbol=%s", c.Param("shipSymbol")))
+	err := ctl.stl.OrbitShip(c.Param("shipSymbol"))
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	err = c.String(http.StatusOK, "{}")
+	if err != nil {
+		c.Logger().Error(err.Error())
+	}
+	return err
+}
+func (ctl *fleetController) dockShip(c echo.Context) error {
+	c.Logger().Info(fmt.Sprintf("Request: POST DockShip shipSymbol=%s", c.Param("shipSymbol")))
+	err := ctl.stl.DockShip(c.Param("shipSymbol"))
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	err = c.String(http.StatusOK, "{}")
 	if err != nil {
 		c.Logger().Error(err.Error())
 	}
 	return err
 }
 
-func (ctl *fleetController) getShips(c echo.Context) error {
-	c.Logger().Info("Request: GET Ships")
-	ships, err := ctl.stl.GetShips()
+func (ctl *fleetController) listShips(c echo.Context) error {
+	c.Logger().Info("Request: LIST Ships")
+	ships, err := ctl.stl.ListShips()
 	if err != nil {
 		c.Logger().Error(err.Error())
 		return c.String(http.StatusInternalServerError, "Error: "+err.Error())
@@ -69,22 +106,43 @@ func (ctl *fleetController) getShip(c echo.Context) error {
 	ship, err := ctl.stl.GetShip(c.Param("shipSymbol"))
 	if err != nil {
 		c.Logger().Error(err.Error())
-		return c.String(http.StatusInternalServerError, "Error: "+err.Error())
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
-	c.Logger().Debugf("--Resp: Ship\n%+v", ship)
 
-	// err = c.Render(http.StatusOK, "myShips", shipsPage{
-	// 	Page: PageData{
-	// 		PageName: "Ship",
-	// 	},
-	// 	Ships: []stlib.Ship{ship},
-	// })
-	err = c.JSONPretty(http.StatusOK, shipsPage{
+	waypoints, err := ctl.stl.ListWaypoints(ship.Nav.WaypointSymbol.System)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	c.Logger().Debugf("Data: Ship\n%+v\nWaypoints\n%+v", ship, waypoints)
+	err = c.Render(http.StatusOK, "ship", shipPage{
 		Page: PageData{
 			PageName: "Ship",
+			JavaScript: []string{
+				"ship",
+			},
 		},
-		Ships: []stlib.Ship{ship},
-	}, "   ")
+		Ship:      ship,
+		Waypoints: waypoints,
+	})
+	if err != nil {
+		c.Logger().Error(err.Error())
+	}
+	return err
+}
+
+func (ctl *fleetController) navigateShip(c echo.Context) error {
+	shipSymbol := c.Param("shipSymbol")
+	waypointSymbol := c.FormValue("waypointSymbol")
+	c.Logger().Info(fmt.Sprintf("Request: POST NavigateShip shipSymbol=%s waypointSymbol=%s", shipSymbol, waypointSymbol))
+	err := ctl.stl.NavigateShip(shipSymbol, waypointSymbol)
+	if err != nil {
+		c.Logger().Error(err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	err = c.String(http.StatusOK, "{}")
 	if err != nil {
 		c.Logger().Error(err.Error())
 	}
